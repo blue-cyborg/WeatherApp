de1bc51077fefbb6cca8e90e0f9291d8848e5824
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2021-07-29 20:58:59 -0400

    followed the shecodes tutorials on js
    scrapped the JavaScript that was in place and did a rebuild.   Followed Matt's tutorials to make better js.  made better search engine, have a city load on go, and copied and pasted the old geolocation stuff in.

--- src/script.js
+++ src/script.js
@@ -1,8 +1,6 @@
-//This is for putting the current date and time outside the gradient display.
-let apiKey = "63b79185d21891dc86fad86aa0ab0b0b";
-
-let now = new Date();
-
+//define the date and time
+function formatDate(timestamp){
+let date = new Date(timestamp);
 let days = [
   "Sunday",
   "Monday",
@@ -28,97 +26,73 @@ let months = [
   "December"
 ];
 
-let day = days[now.getDay()];
-let currentMonth = months[now.getMonth()];
-let currentDate = now.getDate();
-let currentYear = now.getFullYear();
-let time = now.toLocaleTimeString("en-US", {
+let day = days[date.getDay()];
+let currentMonth = months[date.getMonth()];
+let currentDate = date.getDate();
+let currentYear = date.getFullYear();
+let time = date.toLocaleTimeString("en-US", {
   hour: "2-digit",
   minute: "2-digit"
 });
-let dateTime = document.querySelector("#current-data");
-dateTime.innerHTML = `${day}, ${currentMonth} ${currentDate} ${currentYear}, ${time}`;
-
-//Begin search engine section
-//This is what I want to do with that api call.
-//I want to alter the display to give the results of the call.
-function alterDisplayWindows(response) {
-  console.log(response.data.name);
-  document.querySelector("#city-name").innerHTML = response.data.name;
-  let temp = Math.round(response.data.main.temp);
-  console.log(temp);
-  let displayMyTemp = document.querySelector("#read-out");
-  displayMyTemp.innerHTML = `${temp}°F`;
-
-  let humid = response.data.main.humidity;
-  console.log(humid);
-  let displayMyHumidity = document.querySelector("#humidity-read");
-  displayMyHumidity.innerHTML = `Humidity: ${humid}%`;
-
-  let precipitation = Math.round(response.data.main.temp);
-  let currentPrec = document.querySelector("#precip-read");
-  currentPrec.innerHTML = `Precipitation: ${precipitation}%`;
-  
-  let wind = Math.round(response.data.wind.speed);
-  console.log(wind);
-  let displayMyWindsp = document.querySelector("#wind-read");
-  displayMyWindsp.innerHTML = `Wind Speed: ${wind} km/h`;
-
-  let feel = Math.round(response.data.main.feels_like);
-  let feelValue = document.querySelector("#feel-read");
-  feelValue.innerHTML = `It feels like ${feel}°F`;
-
-  let description = response.data.weather[0].main;
-  let describeWeather = document.querySelector("#describe-read");
-  describeWeather.innerHTML = `Currently: ${description}`;
-
-  let locationVisual = document.querySelector("p.weather-visual");
-  if (response.data.weather[0].main === "Clear") {
-    locationVisual.innerHTML = "🌞";
-  }
-  if (response.data.weather[0].main === "Clouds") {
-    locationVisual.innerHTML = "⛅";
-  }
-  if (response.data.weather[0].main === "Drizzle") {
-    locationVisual.innerHTML = "☂️";
-  }
-  if (response.data.weather[0].main === "Thunderstorm") {
-    locationVisual.innerHTML = "⛈️";
-  }
-  if (response.data.weather[0].main === "Rain") {
-    locationVisual.innerHTML = "☂️";
-  }
-  if (response.data.weather[0].main === "Snow") {
-    locationVisual.innerHTML = "❄️";
-  }
+return `${day}, ${currentMonth} ${currentDate} ${currentYear}, ${time}`;
 }
-//Step 2
-//the city name is going to go in the display area as the query selctor allows altering HTML.
-//There is an api call to get weather info.
+//search engine and display infor//
+function displayTemperature(response) {
+ let cityElement = document.querySelector("#city-name");
+ let humid = response.data.main.humidity;
+ let humidityElement = document.querySelector("#humidity-read");
+ let precipitation = Math.round(response.data.main.temp);
+ let precipitationElement = document.querySelector("#precip-read"); 
+ let wind = Math.round(response.data.wind.speed);
+ let windElement = document.querySelector("#wind-read");
+ let feel = Math.round(response.data.main.feels_like);
+ let feelElement = document.querySelector("#feel-read");
+ let temp = Math.round(response.data.main.temp);
+ let temperatureElement = document.querySelector("#read-out");
+ let description = response.data.weather[0].main;
+ let descriptionElement = document.querySelector("#describe-read");
+ let dateElement = document.querySelector("#current-data");
+ let visualElement = document.querySelector("#representation");
 
-function searchEngine(event) {
+ cityElement.innerHTML = response.data.name;
+ humidityElement.innerHTML = `Humidity: ${humid}%`;
+ precipitationElement.innerHTML = `Precipitation: ${precipitation}%`;
+ windElement.innerHTML = `Wind Speed: ${wind} km/h`;
+ feelElement.innerHTML = `It feels like ${feel}°F`;
+ temperatureElement.innerHTML = `${temp}°F`;
+ descriptionElement.innerHTML = `Currently: ${description}`;
+ dateElement.innerHTML = formatDate(response.data.dt * 1000)
+ visualElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+}
+
+function search(city) {
+let apiKey = "63b79185d21891dc86fad86aa0ab0b0b";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+axios.get(apiUrl).then(displayTemperature)
+}
+
+function handleSubmit(event) {
   event.preventDefault();
-  let endUserInput = document.querySelector("#city-input");
-  let cityDisplay = document.querySelector("#city-name");
-  cityDisplay.innerHTML = `${endUserInput.value}`;
-  let city = endUserInput.value;
-  let units = "imperial";
-  let apiUrlSearch = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrlSearch).then(alterDisplayWindows);
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value)
 }
-//Step 1
-//This is where the enduser types in what city they want weather for.
-let searchButton = document.querySelector("#search-city");
-searchButton.addEventListener("submit", searchEngine);
 
-//end search engine button info
+search("Rochester");
 
-//The My Current Location Button
+let form = document.querySelector("#search-city");
+form.addEventListener("submit", handleSubmit);
+//end search engine section//
+
+//geolocation section//
 function retrievePosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
-  axios.get(url).then(alterDisplayWindows);
+  let apiKey = "63b79185d21891dc86fad86aa0ab0b0b";
+  let urlPosition = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+  axios.get(urlPosition).then(displayTemperature);
 }
 function getMyLocation() {
   navigator.geolocation.getCurrentPosition(retrievePosition);
@@ -126,4 +100,4 @@ function getMyLocation() {
 
 let currentLocation = document.querySelector("#your-location");
 currentLocation.addEventListener("click", getMyLocation);
-//end of geolocation section
+//end geolocation section//
